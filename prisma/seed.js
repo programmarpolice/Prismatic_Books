fc9e6d84f64f3869b97f3b91f71f0de9cde34f16
Joy Harrington <joysyk23@gmail.com>
2024-10-10 12:09:06 -0500

    After group

---
+++ prisma/seed.js
@@ -0,0 +1,22 @@
+const prisma = require("../prisma");
+const seed = async () => {
+  const books = [
+    { title: "The Great Gatsby" },
+    { title: "Jane Eyre" },
+    { title: "James and the Giant Peach" },
+    { title: "Pride and Prejudice" },
+    { title: "Sense and Sensibility" },
+    { title: "Phantom of the Opera" },
+    { title: "Tale of Two Cities" },
+    { title: "Drood" },
+    { title: "Where the Wild Things Are" },
+  ];
+  await prisma.book.createMany({ data: books });
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
